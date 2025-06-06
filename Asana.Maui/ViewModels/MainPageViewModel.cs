45ff20539436746bba4a040bf817d36100506394
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2025-06-06 10:52:06 -0400

    Adding ToDo list view to the main page

---
+++ Asana.Maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Asana.Library.Models;
+using Asana.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Maui.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private ToDoServiceProxy _toDoSvc;
+
+        public MainPageViewModel()
+        {
+            _toDoSvc = ToDoServiceProxy.Current;
+        }
+
+        public ObservableCollection<ToDo> ToDos
+        {
+            get
+            {
+                return new ObservableCollection<ToDo>(_toDoSvc.ToDos);
+            }
+        }
+
+        public bool IsShowCompleted { get; set; }
+    }
+}
