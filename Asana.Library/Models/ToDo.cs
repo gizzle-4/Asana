bae176212fa40efa85b653e4155b1549868ee477
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2025-06-13 09:49:01 -0400

    finished implementation of Add ToDo feature closes #19

--- Asana.Library/Models/ToDo.cs
+++ Asana.Library/Models/ToDo.cs
@@ -11,6 +11,7 @@ namespace Asana.Library.Models
         public ToDo()
         {
             Id = 0;
+            IsCompleted = false;
         }
         public string? Name { get; set; }
         public string? Description { get; set; }
