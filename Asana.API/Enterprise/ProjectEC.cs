7117bf2a9e1af7000651a0ab145ee7ad234b6984
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2025-07-07 10:44:04 -0400

    Added server side CRUD support for projects

---
+++ Asana.API/Enterprise/ProjectEC.cs
@@ -0,0 +1,39 @@
+﻿using Asana.API.Database;
+using Asana.Library.Models;
+
+namespace Asana.API.Enterprise
+{
+    public class ProjectEC
+    {
+        public IEnumerable<Project> Get()
+        {
+            return FakeDatabase.Current.Projects.Take(100);
+        }
+
+        public Project? GetById(int id)
+        {
+            return FakeDatabase.Current.Projects.FirstOrDefault(p => p.Id == id);
+        }
+
+        public Project? AddOrUpdate(Project? project)
+        {
+            if(project == null)
+            {
+                return project;
+            }
+
+            FakeDatabase.Current.AddOrUpdateProject(project);
+            return project;
+        }
+
+        public Project? Delete(int id)
+        {
+            var projectToDelete = GetById(id);
+            if (projectToDelete != null)
+            {
+                FakeDatabase.Current.DeleteProject(projectToDelete);
+            }
+            return projectToDelete;
+        }
+    }
+}
